99cb43c96e892fa034862f50d6a29f645ebde417
Iavor Diatchki <diatchki@galois.com>
2010-04-13 17:43:49 -0700

    Use new type in types

--- Data/Tree/Zipper.hs
+++ Data/Tree/Zipper.hs
@@ -67,12 +67,12 @@ newtype Full a  = F { unF :: Tree a }
 tree x = unF (content x)
 
 
-type TreeLoc    = TreePos Full
+type TreeLoc = TreePos Full
 
 -- Moving around ---------------------------------------------------------------
 
 -- | The parent of the given location.
-parent :: TreeLoc a -> Maybe (TreeLoc a)
+parent :: TreePos Full a -> Maybe (TreePos Full a)
 parent loc =
   case parents loc of
     (pls,v,prs) : ps -> Just
@@ -83,19 +83,19 @@ parent loc =
 
 
 -- | The top-most parent of the given location.
-root :: TreeLoc a -> TreeLoc a
+root :: TreePos Full a -> TreePos Full a
 root loc = maybe loc root (parent loc)
 
 
 -- | The left sibling of the given location.
-left :: TreeLoc a -> Maybe (TreeLoc a)
+left :: TreePos Full a -> Maybe (TreePos Full a)
 left loc =
   case lefts loc of
     t : ts -> Just loc { content = F t, lefts = ts, rights = tree loc : rights loc }
     []     -> Nothing
 
 -- | The right sibling of the given location.
-right :: TreeLoc a -> Maybe (TreeLoc a)
+right :: TreePos Full a -> Maybe (TreePos Full a)
 right loc =
   case rights loc of
     t : ts -> Just loc { content = F t, lefts = tree loc : lefts loc, rights = ts }
@@ -103,7 +103,7 @@ right loc =
 
 
 -- | The first child of the given location.
-firstChild :: TreeLoc a -> Maybe (TreeLoc a)
+firstChild :: TreePos Full a -> Maybe (TreePos Full a)
 firstChild loc =
   case subForest (tree loc) of
     t : ts -> Just
@@ -111,7 +111,7 @@ firstChild loc =
     [] -> Nothing
 
 -- | The last child of the given location.
-lastChild :: TreeLoc a -> Maybe (TreeLoc a)
+lastChild :: TreePos Full a -> Maybe (TreePos Full a)
 lastChild loc =
   case reverse (subForest (tree loc)) of
     t : ts -> Just
@@ -119,13 +119,13 @@ lastChild loc =
     [] -> Nothing
 
 -- | The child with the given index (starting from 0).
-getChild :: Int -> TreeLoc a -> Maybe (TreeLoc a)
+getChild :: Int -> TreePos Full a -> Maybe (TreePos Full a)
 getChild n loc =
   do (ls,t,rs) <- splitChildren (subForest (tree loc)) n
      return Loc { content = F t, lefts = ls, rights = rs, parents = downParents loc }
 
 -- | The first child that satisfies a predicate.
-findChild :: (Tree a -> Bool) -> TreeLoc a -> Maybe (TreeLoc a)
+findChild :: (Tree a -> Bool) -> TreePos Full a -> Maybe (TreePos Full a)
 findChild p loc =
   do (ls,t,rs) <- split [] (subForest (tree loc))
      return Loc { content = F t, lefts = ls, rights = rs, parents = downParents loc }
@@ -135,7 +135,7 @@ findChild p loc =
         split _ []              = Nothing
 
 -- | private: computes the parent for "down" operations.
-downParents :: TreeLoc a -> [(Forest a, a, Forest a)]
+downParents :: TreePos Full a -> [(Forest a, a, Forest a)]
 downParents loc = (lefts loc, rootLabel (tree loc), rights loc) : parents loc
 
 
@@ -143,51 +143,51 @@ downParents loc = (lefts loc, rootLabel (tree loc), rights loc) : parents loc
 -- Conversions -----------------------------------------------------------------
 
 -- | A location corresponding to the root of the given tree.
-fromTree :: Tree a -> TreeLoc a
+fromTree :: Tree a -> TreePos Full a
 fromTree t = Loc { content = F t, lefts = [], rights = [], parents = [] }
 
 -- | The location of the first tree in a forest.
-fromForest :: Forest a -> Maybe (TreeLoc a)
+fromForest :: Forest a -> Maybe (TreePos Full a)
 fromForest (t:ts) = Just Loc { content = F t, lefts = [], rights = ts, parents = [] }
 fromForest []     = Nothing
 
 -- | Computes the tree containing this location.
-toTree :: TreeLoc a -> Tree a
+toTree :: TreePos Full a -> Tree a
 toTree loc = tree (root loc)
 
 -- | Computes the forest containing this location.
-toForest :: TreeLoc a -> Forest a
+toForest :: TreePos Full a -> Forest a
 toForest loc = let r = root loc in combChildren (lefts r) (tree r) (rights r)
 
 
 -- Queries ---------------------------------------------------------------------
 
 -- | Are we at the top of the tree?
-isRoot :: TreeLoc a -> Bool
+isRoot :: TreePos Full a -> Bool
 isRoot loc = null (parents loc)
 
 -- | Are we at the left end of the the tree?
-isFirst :: TreeLoc a -> Bool
+isFirst :: TreePos Full a -> Bool
 isFirst loc = null (lefts loc)
 
 -- | Are we at the right end of the tree?
-isLast :: TreeLoc a -> Bool
+isLast :: TreePos Full a -> Bool
 isLast loc = null (rights loc)
 
 -- | Are we at the bottom of the tree?
-isLeaf :: TreeLoc a -> Bool
+isLeaf :: TreePos Full a -> Bool
 isLeaf loc = null (subForest (tree loc))
 
 -- | Do we have a parent?
-isChild :: TreeLoc a -> Bool
+isChild :: TreePos Full a -> Bool
 isChild loc = not (isRoot loc)
 
 -- | Get the node index inside the sequence of children
-getNodeIndex :: TreeLoc a -> Int
+getNodeIndex :: TreePos Full a -> Int
 getNodeIndex loc = length (lefts loc)
 
 -- | Do we have children?
-hasChildren :: TreeLoc a -> Bool
+hasChildren :: TreePos Full a -> Bool
 hasChildren loc = not (isLeaf loc)
 
 
@@ -195,23 +195,23 @@ hasChildren loc = not (isLeaf loc)
 
 
 -- | Change the current tree.
-setTree :: Tree a -> TreeLoc a -> TreeLoc a
+setTree :: Tree a -> TreePos Full a -> TreePos Full a
 setTree t loc = loc { content = F t }
 
 -- | Modify the current tree.
-modifyTree :: (Tree a -> Tree a) -> TreeLoc a -> TreeLoc a
+modifyTree :: (Tree a -> Tree a) -> TreePos Full a -> TreePos Full a
 modifyTree f loc = setTree (f (tree loc)) loc
 
 -- | Modify the label at the current node.
-modifyLabel :: (a -> a) -> TreeLoc a -> TreeLoc a
+modifyLabel :: (a -> a) -> TreePos Full a -> TreePos Full a
 modifyLabel f loc = setLabel (f (getLabel loc)) loc
 
 -- | Change the label at the current node.
-setLabel :: a -> TreeLoc a -> TreeLoc a
+setLabel :: a -> TreePos Full a -> TreePos Full a
 setLabel v loc = modifyTree (\t -> t { rootLabel = v }) loc
 
 -- Get the current label.
-getLabel :: TreeLoc a -> a
+getLabel :: TreePos Full a -> a
 getLabel loc = rootLabel (tree loc)
 
 
@@ -219,24 +219,24 @@ getLabel loc = rootLabel (tree loc)
 
 -- | Insert a tree to the left of the current position.
 -- The new tree becomes the current tree.
-insertLeft :: Tree a -> TreeLoc a -> TreeLoc a
+insertLeft :: Tree a -> TreePos Full a -> TreePos Full a
 insertLeft t loc = loc { content = F t, rights = tree loc : rights loc }
 
 -- | Insert a tree to the right of the current position.
 -- The new tree becomes the current tree.
-insertRight :: Tree a -> TreeLoc a -> TreeLoc a
+insertRight :: Tree a -> TreePos Full a -> TreePos Full a
 insertRight t loc = loc { content = F t, lefts = tree loc : lefts loc }
 
 -- | Insert a tree as the first child of the current position.
 -- The new tree becomes the current tree.
-insertDownFirst :: Tree a -> TreeLoc a -> TreeLoc a
+insertDownFirst :: Tree a -> TreePos Full a -> TreePos Full a
 insertDownFirst t loc =
   loc { content = F t, lefts = [], rights = subForest (tree loc)
       , parents = downParents loc }
 
 -- | Insert a tree as the last child of the current position.
 -- The new tree becomes the current tree.
-insertDownLast :: Tree a -> TreeLoc a -> TreeLoc a
+insertDownLast :: Tree a -> TreePos Full a -> TreePos Full a
 insertDownLast t loc =
   loc { content = F t, lefts = reverse (subForest (tree loc)), rights = []
       , parents = downParents loc }
@@ -244,7 +244,7 @@ insertDownLast t loc =
 -- | Insert a tree as a particular child of the current location.
 -- Children are numbered from 0.
 -- The new tree becomes the current tree.
-insertDownAt :: Int -> Tree a -> TreeLoc a -> Maybe (TreeLoc a)
+insertDownAt :: Int -> Tree a -> TreePos Full a -> Maybe (TreePos Full a)
 insertDownAt n t loc =
   do (ls,x,rs) <- splitChildren (subForest (tree loc)) n
      return Loc { content = F t, lefts = ls, rights = x : rs
@@ -254,7 +254,7 @@ insertDownAt n t loc =
 --   * the right sibling, or if none
 --   * the left sibling, or if none
 --   * the parent.
-delete :: TreeLoc a -> Maybe (TreeLoc a)
+delete :: TreePos Full a -> Maybe (TreePos Full a)
 delete loc =
   case rights loc of
     t : ts -> Just loc { content = F t, rights = ts }
