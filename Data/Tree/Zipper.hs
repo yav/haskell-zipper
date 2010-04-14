14abcc1f51d0f80564e956e0841fdb9532032646
Iavor S. Diatchki <diatchki@galois.com>
2010-04-14 11:22:12 -0700

    Provide custom functions, instead of exporting the record selectors.
    It would appear that exporting the record selectors allows for _updateing_
    as well as selection which breaks the abstraction.  In this way,
    we can also be more uniform over the type of positions, as all functions
    are qualified-polymorphic over the type of position.

--- Data/Tree/Zipper.hs
+++ Data/Tree/Zipper.hs
@@ -51,12 +51,24 @@ import Prelude hiding (last)
 -- a specific tree (if 't' is 'Full'), or if it is pointing in-between
 -- trees (if 't' is 'Empty').
 data TreePos t a  = Loc
-  { content   :: t a        -- ^ The currently selected tree.
-  , before    :: Forest a     -- ^ Siblings before this position, closest first.
-  , after     :: Forest a     -- ^ Siblings after this position, closest first.
-  , parents   :: [(Forest a, a, Forest a)]
-      -- ^ The contexts of the parents for this position.
+  { _content   :: t a        -- ^ The currently selected tree.
+  , _before    :: Forest a
+  , _after     :: Forest a
+  , _parents   :: [(Forest a, a, Forest a)]
   } deriving (Read,Show,Eq)
+
+
+-- | Siblings before this position, closest first.
+before         :: PosType t => TreePos t a -> Forest a
+before          = _before
+
+-- | Siblings after this position, closest first.
+after          :: PosType t => TreePos t a -> Forest a
+after           = _after
+
+-- | The contexts of the parents for this position.
+parents        :: PosType t => TreePos t a -> [(Forest a, a, Forest a)]
+parents         = _parents
 
 -- | Position which does not point to a tree (e.g., it is between two trees).
 data Empty a    = E deriving (Read,Show,Eq)
@@ -111,10 +123,10 @@ parent :: PosType t => TreePos t a -> Maybe (TreePos Full a)
 parent loc =
   case parents loc of
     (ls,a,rs) : ps -> Just
-      Loc { content = F (Node a (forest loc))
-          , before   = ls
-          , after  = rs
-          , parents = ps
+      Loc { _content = F (Node a (forest loc))
+          , _before   = ls
+          , _after  = rs
+          , _parents = ps
           }
     [] -> Nothing
 
@@ -125,52 +137,52 @@ root loc = maybe loc root (parent loc)
 
 -- | The space immediately before this location.
 prevSpace :: TreePos Full a -> TreePos Empty a
-prevSpace loc = loc { content = E, after = tree loc : after loc }
+prevSpace loc = loc { _content = E, _after = tree loc : after loc }
 
 -- | The tree before this location, if any.
 prevTree :: TreePos Empty a -> Maybe (TreePos Full a)
 prevTree loc =
   case before loc of
-    t : ts -> Just loc { content = F t, before = ts }
+    t : ts -> Just loc { _content = F t, _before = ts }
     []     -> Nothing
 
 
 -- | The space immediately after this location.
 nextSpace :: TreePos Full a -> TreePos Empty a
-nextSpace loc = loc { content = E, before = tree loc : before loc }
+nextSpace loc = loc { _content = E, _before = tree loc : before loc }
 
 
 -- | The tree after this location, if any.
 nextTree :: TreePos Empty a -> Maybe (TreePos Full a)
 nextTree loc =
   case after loc of
-    t : ts -> Just loc { content = F t, after = ts }
+    t : ts -> Just loc { _content = F t, _after = ts }
     []     -> Nothing
 
 
 -- | The location at the beginning of the forest of children.
 children :: TreePos Full a -> TreePos Empty a
 children loc =
-  Loc { content = E
-      , before   = []
-      , after  = subForest (tree loc)
-      , parents = (before loc, rootLabel (tree loc), after loc)
-                : parents loc
+  Loc { _content  = E
+      , _before   = []
+      , _after    = subForest (tree loc)
+      , _parents  = (before loc, rootLabel (tree loc), after loc)
+                  : parents loc
       }
 
 -- | The first space in the current forest.
 first :: TreePos Empty a -> TreePos Empty a
-first loc = loc { content = E
-                , before   = []
-                , after  = reverse (before loc) ++ after loc
+first loc = loc { _content  = E
+                , _before   = []
+                , _after    = reverse (before loc) ++ after loc
                 }
 
 -- | The last space in the current forest.
 last :: TreePos Empty a -> TreePos Empty a
-last loc = loc { content = E
-                    , before   = reverse (after loc) ++ before loc
-                    , after  = []
-                    }
+last loc = loc { _content = E
+               , _before  = reverse (after loc) ++ before loc
+               , _after   = []
+               }
 
 
 -- | The first child of the given location.
@@ -190,11 +202,11 @@ lastChild = prevTree . last . children
 
 -- | A location corresponding to the root of the given tree.
 fromTree :: Tree a -> TreePos Full a
-fromTree t = Loc { content = F t, before = [], after = [], parents = [] }
+fromTree t = Loc { _content = F t, _before = [], _after = [], _parents = [] }
 
 -- | The location at the beginning of the forest.
 fromForest :: Forest a -> TreePos Empty a
-fromForest ts = Loc { content = E, before = [], after = ts, parents = [] }
+fromForest ts = Loc { _content = E, _before = [], _after = ts, _parents = [] }
 
 -- | The tree containing this location.
 toTree :: TreePos Full a -> Tree a
@@ -214,11 +226,11 @@ isRoot :: PosType t => TreePos t a -> Bool
 isRoot loc = null (parents loc)
 
 -- | Are we the first position (of its kind) in a forest.
-isFirst :: TreePos t a -> Bool
+isFirst :: PosType t => TreePos t a -> Bool
 isFirst loc = null (before loc)
 
 -- | Are we the last position (of its kind) in a forest.
-isLast :: TreePos t a -> Bool
+isLast :: PosType t => TreePos t a -> Bool
 isLast loc = null (after loc)
 
 -- | Are we at the bottom of the tree?
@@ -239,7 +251,7 @@ hasChildren loc = not (isLeaf loc)
 
 -- | The selected tree.
 tree :: TreePos Full a -> Tree a
-tree x = unF (content x)
+tree x = unF (_content x)
 
 -- | The current label.
 label :: TreePos Full a -> a
@@ -247,17 +259,17 @@ label loc = rootLabel (tree loc)
 
 -- | Insert a new tree at the current position.
 insert :: Tree a -> TreePos Empty a -> TreePos Full a
-insert t loc = loc { content = F t }
+insert t loc = loc { _content = F t }
 
 -- | Remove the tree at the current position.
 delete :: TreePos Full a -> TreePos Empty a
-delete loc = loc { content = E }
+delete loc = loc { _content = E }
 
 
 
 -- | Change the current tree.
 setTree :: Tree a -> TreePos Full a -> TreePos Full a
-setTree t loc = loc { content = F t }
+setTree t loc = loc { _content = F t }
 
 -- | Modify the current tree.
 modifyTree :: (Tree a -> Tree a) -> TreePos Full a -> TreePos Full a
